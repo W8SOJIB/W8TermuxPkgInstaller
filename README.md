8103010a5683b0bccc0c82ca1835ce17dbc88dd6
W8SOJIB <92366482+W8SOJIB@users.noreply.github.com>
2023-01-31 22:36:39 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Termux-All-Packages-Install
+All Packages Install On Termux In One Click
+git clone https://github.com/W8SOJIB/W8TermuxPkgInstaller
+cd W8TermuxPkgInstaller
+./install.sh
---
+++ install.sh
@@ -0,0 +1,64 @@
+#!/data/data/com.termux/files/usr/bash
+echo $red 
+ echo ""----------------------------------------------------------------------------- ""
+echo " This Script is Made by W8SOJIB  "
+echo "---------------------------------------------------------------------------------"
+
+echo
+
+echo  "#####################################"
+echo   "  https://www.instagram.com/w8_sojib/ "               
+ echo   " https://github.com/W8SOJIB "                                                                    
+echo   "####################################"
+
+echo
+echo
+echo
+echo
+
+echo "."
+
+apt update -y                      #update in progress
+apt install python -y           #installation......
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+
+echo  "#####################################"
+echo   "  Follow Me On Facebook     "               
+ echo   " https://www.facebook.com/W8Vai/ "                                                                    
+echo   "####################################"
+
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+
+echo  "#####################################"
+echo   "  Follow Me On Instagram  "             
+ echo   " https://www.instagram.com/w8_sojib/ "                                                                    
+echo   "####################################"
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+
+echo  "#####################################"
+echo   "  Follow Me On Facebook  "               
+ echo   " https://www.facebook.com/W8Vai/ "                                                                    
+echo   "####################################"
+
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install unzip -y
+apt install tor -y
+apt install wget -y
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+echo " If You Connected To Internet Then  "
+echo " YOUR PACKAGES IS INSTALLED SUCCESSFULLY "
+
+exit
